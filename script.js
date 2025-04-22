20d8a8087d24691e23f704ae8457e622e13b0652
3vdi <unitcomics.world@proton.me>
2025-04-22 18:20:49 +0530

    cool cursor

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-gsap.to("h1", {
-    duration: 2,
-    delay:1,
-    rotation: 360,
-    color: "blue", // 👈 Add this line
-    ease: "power1.inOut",
-    repeat: 1,
-    yoyo: true,
-  });
+// gsap.to("h1", {
+//     duration: 2,
+//     delay:1,
+//     rotation: 360,
+//     color: "blue", // 👈 Add this line
+//     ease: "power1.inOut",
+//     repeat: 1,
+//     yoyo: true,
+//   });
+
+var cursor = document.querySelector("#cursor")
+var img = document.querySelector("#image")
+document.querySelector("#box").addEventListener("mousemove", function(dets) {
+    gsap.to(cursor, {
+        duration: 0.5,
+        x: dets.x,
+        y: dets.y,
+        ease: "back.out(1.7)",
+    })
+  })
+
+  img.addEventListener("mouseenter", function(dets) {
+    gsap.to(cursor, {
+        scale:3,
+        duration: 0.5,
+    })
+  })
+
+  img.addEventListener("mouseleave", function(dets) {
+    gsap.to(cursor, {
+        scale:1,
+        duration: 0.5,
+    })
+  })
   
+
